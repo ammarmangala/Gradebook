95e030213e1c731d659def0ea6715479e549f85d
hervemangalakitungwa <herve.mangala@gmail.com>
2019-09-03 22:11:10 +0200

    update

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,12 +7,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-
             var book = new Book("Ammar's Grade Book");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
-            book.ShowStatistics();
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
         }
     }
 }
