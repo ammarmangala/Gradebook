c688c3ce88cfc01c70bbc538e79ee52fe6db8005
hervemangalakitungwa <herve.mangala@gmail.com>
2019-09-03 21:18:05 +0200

    update

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //  arange
+           
+
+            // act
+            var actual = x + y;
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
