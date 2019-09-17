324e68ae682cfde56568ee209915bfde6908e831
Ammar <herve.mangala@gmail.com>
2019-09-17 18:06:34 +0200

    update

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,83 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void ValueTypesAlsoPassByValue()
+        {
+           var x = GetInt();
+           SetInt(ref x);
+
+           Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int z)
+        {
+            z = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(out book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetName(out Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Ammar";
+            var upper = MakeUpperCase(name);
+
+            Assert.Equal("Ammar", name);
+            Assert.Equal("AMMAR", upper);
+        }
+ 
+        private string MakeUpperCase(string parameter)
+        {
+            return parameter.ToUpper();
+        }
+
+        [Fact]
         public void GetBookRetursDifferentObjects()
         {
             var book1 = GetBook("Book 1");
