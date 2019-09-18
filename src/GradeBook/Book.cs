7de123e56f8e8ab93a844f85c9e99301bf754626
Ammar <herve.mangala@gmail.com>
2019-09-18 14:36:01 +0200

    update

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -19,9 +19,32 @@ namespace GradeBook
         }
     }
 
-    public class Book : NamedObject
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
+    public abstract class Book : NamedObject, IBook
     {
         public Book(string name) : base(name)
+        {
+        }
+
+        public event GradeAddedDelegate GradeAdded;
+
+        public abstract void AddGrade(double grade);
+
+        public Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class InMemoryBook : Book
+    {
+        public InMemoryBook(string name) : base(name)
         {
             grades = new List<double>();
             Name = name;
@@ -49,7 +72,7 @@ namespace GradeBook
            }
         }
 
-        public void AddGrade(double grade)
+        public override void AddGrade(double grade)
         {
             if(grade <= 100 && grade >= 0)
             {
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,47 +7,52 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book("Ammar's Grade Book");
+            var book = new InMemoryBook("Ammar's Grade Book");
             book.GradeAdded += OnGradeAdded;
 
-            while(true)
-            {
-                Console.WriteLine("Enter a grade or 'q' to quit");
-                var input = Console.ReadLine();
-
-                if(input == "q")
-                {
-                    break;
-                }
-
-                try 
-                {
-                    var grade = double.Parse(input);
-                    book.AddGrade(grade);
-
-                }
-                catch(ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                catch(FormatException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                   Console.WriteLine("**");
-                }
-            }
+            EnterGrades(book);
 
             var stats = book.GetStatistics();
 
-            Console.WriteLine(Book.CATEGORY);
+            Console.WriteLine(InMemoryBook.CATEGORY);
             Console.WriteLine($"For the book named {book.Name}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The letter grade is {stats.letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
         }
 
         static void OnGradeAdded(object sender, EventArgs e)
